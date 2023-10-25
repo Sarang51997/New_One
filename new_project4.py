7e13fe2657a27de514eb2586dc2c6043a0a93f28
Sarang51997 <sarangkumbhar5@gmail.com>
2023-10-25 23:59:12 +0530

    new project 4

---
+++ new_project4.py
@@ -0,0 +1,8 @@
+a = 33
+b = 200
+if is a build-in keyword
+if a > b: #coloan
+    print("b is greater than a") # 4spaces = 1 tab
+    print("hello")
+else: #else block or else statement
+    print("I will be always executed..if IF statement is incorrect")
